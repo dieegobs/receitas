6db228e9ec63b61400c96fdb29730f4f081124cc
dieegobs <diegohevant@gmail.com>
2023-04-04 23:19:02 -0300

    Fazendo a base

--- App.js
+++ App.js
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
-export default function App() {
-  return (
+export default function App(){
+  return(
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Meu Primeiro App!!!</Text>
+      <Nome nome="Diego Bittencourt"/>
+      <Nome nome="Junior dos Santos"/>
+
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    container:{ 
+      backgroundColor: "red",
+      height: 400
+    },
+    title: {
+      fontSize: 24,
+      marginTop: 100,
+      color: "white"
+    }
+})
+
+
+function Nome ({nome}){
+  return(
+    <Text>Olá, {nome}</Text>
+  )
+}
