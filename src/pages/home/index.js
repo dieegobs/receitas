e5c9c0e465d57904806f884554c165b5f1af2399
dieegobs <diegohevant@gmail.com>
2023-04-05 23:36:11 -0300

    Fazendo o search

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,22 +1,60 @@
-import {View, Text, StyleSheet, SafeAreaView } from 'react-native'
+import { View, Text, StyleSheet, SafeAreaView, TextInput, TouchableOpacity } from 'react-native'
+import { Ionicons } from '@expo/vector-icons'
 
 import { Logo } from '../../components/logo'
 
-export function Home(){
-    return(
+export function Home() {
+    return (
         <SafeAreaView style={styles.container}>
-            <Logo/>
-            
+            <Logo />
+            <Text style={styles.title}>Encontre a Receita</Text>
+            <Text style={styles.title}>Que combina com Você</Text>
+
+            <View style={styles.form}>
+                <TextInput
+                    placeholder='Digite o Nome da Comida...'
+                    style={styles.input}
+                />
+                <TouchableOpacity>
+                    <Ionicons name="search" size={28} color="#4CBE6C"/>
+                </TouchableOpacity>
+
+            </View>
+
         </SafeAreaView>
     )
 }
 
 const styles = StyleSheet.create({
-    container: {   
-      flex:1,
-      backgroundColor: '#F3F9FF',
-      paddingTop: 36,
-      paddingStart: 14,
-      paddingEnd: 14,
+    container: {
+        flex: 1,
+        backgroundColor: '#F3F9FF',
+        paddingTop: 36,
+        paddingStart: 14,
+        paddingEnd: 14,
+    },
+    title: {
+        fontSize: 26,
+        fontWeight: "bold",
+        color: "#0e0e0e"
+    },
+    form: {
+        backgroundColor: "#FFF",
+        width: '100%',
+        borderRadius: 8,
+        marginTop: 16,
+        marginBottom: 16,
+        borderWidth: 1,
+        borderColor: "#ECECEC",
+        paddingLeft: 8, 
+        paddingRight: 8, 
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between'
+    },
+    input: {
+        width: '90%',
+        maxWidth: '90%',
+        height: 54
     }
 })
